f7dbd141cca5bb0dbf52d283003238446c9b5672
Yash-WB <yash.harad@wonderbiz.in>
2024-07-10 10:55:51 +0530

    added pass the pillow

---
+++ PassThePillow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_ProblemsDa2
+{
+    class PassThePillow
+    {
+        public static int PassTheP(int n, int time)
+        {
+            int person = 1;
+            int current_time = 0;
+            for (int i = 1; i <= time; i++)
+            {
+                person++;
+                current_time++;
+                if (time == current_time)
+                {
+                    return person;
+                }
+                if (person == n)
+                {
+                    while (person != 1 && time != current_time)
+                    {
+                        person--;
+                        current_time++;
+                        if (time == current_time)
+                        {
+                            return person;
+                        }
+                    }
+                }
+            }
+            return person;
+        }
+        static void Main(string[] args)
+        {
+            int n = 4;
+            int time = 5;
+
+            Console.WriteLine(PassThePillow.PassTheP(n, time));
+        }
+    }
+}
